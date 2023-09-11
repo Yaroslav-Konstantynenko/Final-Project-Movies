bf1cfe16cc3cc49e5b89f1a257f149bedefbda05
Yaroslav Konstantynenko <>
2023-09-11 18:29:45 +0300

    Add Modules

---
+++ FinalProjectMovies/FinalProjectMovies/Modules/MainTabBar/Controllers/MainTabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  MainTabBarController.swift
+//  FinalProjectMovies
+//
+//  Created by Yaroslav Konstantynenko on 11.09.2023.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //generateTabBar()
+    }
+    
+    //private generateTabBar() {
+         
+    //}
+}
