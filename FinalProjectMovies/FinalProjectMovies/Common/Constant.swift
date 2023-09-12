080f213b12847c1b747508da2aba3607fe6d4fa6
Yaroslav Konstantynenko <>
2023-09-12 20:28:44 +0300

    Add youtube and SDWebImage for SPM

---
+++ FinalProjectMovies/FinalProjectMovies/Common/Constant.swift
@@ -0,0 +1,17 @@
+//
+//  Constant.swift
+//  FinalProjectMovies
+//
+//  Created by Yaroslav Konstantynenko on 12.09.2023.
+//
+
+import Foundation
+
+struct Constant {
+    
+    struct network {
+        static let apiKey = "331e07208d153f576f7451e4777990f2"
+        
+        static let popularMovieDay = "https://api.themoviedb.org/3/trending/movie/day"
+    }
+}
