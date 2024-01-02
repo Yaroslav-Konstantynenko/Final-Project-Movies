2fe71acf4b46e44ae52e7e7e3ce3807e78bfec9a
Yaroslav Konstantynenko <konstantynenko.yaroslav@gmail.com>
2024-01-02 14:38:16 +0200

    Refactored code test

--- FinalProjectMovies/FinalProjectMovies/Network/Alamofire/GetNetworkData.swift
+++ FinalProjectMovies/FinalProjectMovies/Network/Alamofire/GetNetworkData.swift
@@ -7,11 +7,37 @@
 
 import UIKit
 import Alamofire
+import SystemConfiguration
 
 class GetNetworkData {
     
     static let shered = GetNetworkData()
     private init() {}
+    
+    
+    func isInternetAvailable() -> Bool {
+            var zeroAddress = sockaddr_in()
+            zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+            zeroAddress.sin_family = sa_family_t(AF_INET)
+
+            guard let reachability = withUnsafePointer(to: &zeroAddress, {
+                $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                    SCNetworkReachabilityCreateWithAddress(nil, $0)
+                }
+            }) else {
+                return false
+            }
+
+            var flags: SCNetworkReachabilityFlags = []
+            if !SCNetworkReachabilityGetFlags(reachability, &flags) {
+                return false
+            }
+
+            let isReachable = flags.contains(.reachable)
+            let needsConnection = flags.contains(.connectionRequired)
+
+            return isReachable && !needsConnection
+        }
     
     //MARK: Get Trending Movies Day
     
